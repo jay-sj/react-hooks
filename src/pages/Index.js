cdbf802b8daa6a01838832e2c9866a5e299453fe
shenqu <80699827@qq.com>
2020-04-26 16:10:16 +0800

    利用useMemo解决性能问题

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,22 +1,52 @@
 /*
- * @Description:UI主组件-利用useReducer代替Redux
+ * @Description:useMemo解决性能问题
  * @Autor: 神曲
  * @Date: 2020-04-26 14:22:09
  * @LastEditors: 神曲
- * @LastEditTime: 2020-04-26 14:58:13
+ * @LastEditTime: 2020-04-26 16:06:17
  */
-import React from "react";
-import ShowArea from "./showArea";
-import Buttons from "./Buttons";
-import { Color } from "./Color";
-const Index = () => {
+import React, { useState, useMemo } from "react";
+// 父组件
+function Index() {
+  const [xiaohong, setXiaohong] = useState("小红待客状态");
+  const [zhiling, setZhiling] = useState("志玲待客状态");
   return (
     <div>
-      <Color>
-        <ShowArea />
-        <Buttons />
-      </Color>
+      <button
+        onClick={() => {
+          setXiaohong(new Date().getTime());
+        }}
+      >
+        小红
+      </button>
+      <button
+        onClick={() => {
+          setZhiling(new Date().getTime() + "志玲走来了");
+        }}
+      >
+        志玲
+      </button>
+      <ChildComponent name={xiaohong}>{zhiling}</ChildComponent>
     </div>
   );
-};
+}
+// 子组件
+function ChildComponent({ name, children }) {
+  function changeXiaohong(name) {
+    console.log("小红来了");
+    return name + ",小红向我们走来了";
+  }
+  // useMemo解决性能问题 防止父组件每次改变导致子组件也执行，消耗性能
+  const actionXiaohong = useMemo(() => {
+    changeXiaohong(name);
+  }, [name]);
+  // 原始写法
+  // const actionXiaohong =changeXiaohong(name);
+  return (
+    <div>
+      <div>{actionXiaohong}</div>
+      <div>{children}</div>
+    </div>
+  );
+}
 export default Index;
