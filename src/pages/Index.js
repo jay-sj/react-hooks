224c71f35d6180e92a57c7050b292e0ab5f765af
shenqu <80699827@qq.com>
2020-04-27 09:45:05 +0800

    使用useMemo解决性能问题

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,51 +1,37 @@
 /*
- * @Description:useMemo解决性能问题
+ * @Description:useRef用法
  * @Autor: 神曲
  * @Date: 2020-04-26 14:22:09
  * @LastEditors: 神曲
- * @LastEditTime: 2020-04-26 16:06:17
+ * @LastEditTime: 2020-04-26 18:15:37
  */
-import React, { useState, useMemo } from "react";
-// 父组件
+import React, { useRef, useState, useEffect } from "react";
 function Index() {
-  const [xiaohong, setXiaohong] = useState("小红待客状态");
-  const [zhiling, setZhiling] = useState("志玲待客状态");
+  // 1、useRef获取jsx dom
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    inputEl.current.value = "Hello";
+    console.log(inputEl);
+  };
+  const [text, setText] = useState("周杰伦");
+  // 2、useRef保存变量
+  const textRef = useRef();
+  useEffect(() => {
+    textRef.current = text;
+    console.log("textRef.current", textRef.current);
+  });
   return (
     <div>
-      <button
-        onClick={() => {
-          setXiaohong(new Date().getTime());
+      <input ref={inputEl} type='text' />
+      <button onClick={onButtonClick}>在input上展示文字</button>
+      <br />
+      <br />
+      <input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
         }}
-      >
-        小红
-      </button>
-      <button
-        onClick={() => {
-          setZhiling(new Date().getTime() + "志玲走来了");
-        }}
-      >
-        志玲
-      </button>
-      <ChildComponent name={xiaohong}>{zhiling}</ChildComponent>
-    </div>
-  );
-}
-// 子组件
-function ChildComponent({ name, children }) {
-  function changeXiaohong(name) {
-    console.log("小红来了");
-    return name + ",小红向我们走来了";
-  }
-  // useMemo解决性能问题 防止父组件每次改变导致子组件也执行，消耗性能
-  const actionXiaohong = useMemo(() => {
-    changeXiaohong(name);
-  }, [name]);
-  // 原始写法
-  // const actionXiaohong =changeXiaohong(name);
-  return (
-    <div>
-      <div>{actionXiaohong}</div>
-      <div>{children}</div>
+      />
     </div>
   );
 }
