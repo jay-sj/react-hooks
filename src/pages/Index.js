c6f2c85ec9bee216c92e3a0cdd50448ef2036e6f
shenqu <80699827@qq.com>
2020-04-27 10:35:15 +0800

    自定义hooks函数

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,37 +1,41 @@
 /*
- * @Description:useRef用法
+ * @Description:自定义hooks函数
  * @Autor: 神曲
  * @Date: 2020-04-26 14:22:09
  * @LastEditors: 神曲
- * @LastEditTime: 2020-04-26 18:15:37
+ * @LastEditTime: 2020-04-27 10:17:40
  */
-import React, { useRef, useState, useEffect } from "react";
-function Index() {
-  // 1、useRef获取jsx dom
-  const inputEl = useRef(null);
-  const onButtonClick = () => {
-    inputEl.current.value = "Hello";
-    console.log(inputEl);
-  };
-  const [text, setText] = useState("周杰伦");
-  // 2、useRef保存变量
-  const textRef = useRef();
-  useEffect(() => {
-    textRef.current = text;
-    console.log("textRef.current", textRef.current);
+import React, { useState, useEffect, useCallback } from "react";
+// 起名 必须要用use开头
+function useWinSize() {
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
   });
+  // useCallback缓存整个方法
+  const onResize = useCallback(() => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    });
+  }, []);
+  // 使用useEffect代替生命周期,监听窗口变化
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    // 取消监听
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+  // 只需要他执行一遍，所以第二个参数放个空数组
+
+  return size;
+}
+function Index() {
+  const size = useWinSize();
   return (
     <div>
-      <input ref={inputEl} type='text' />
-      <button onClick={onButtonClick}>在input上展示文字</button>
-      <br />
-      <br />
-      <input
-        value={text}
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
-      />
+      页面Size:{size.width}X{size.height}
     </div>
   );
 }
